7bde24d83d79ac016b2b76a37df0af99d6f004f0
Valeri <uxenos3@gmail.com>
2023-02-10 14:56:21 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from src.routes import contacts, auth
+
+
+app = FastAPI()
+
+
+app.include_router(contacts.router, prefix='/api')
+app.include_router(auth.router, prefix='/api')
+
+
+@app.get("/")
+def read_root():
+    return {"Welcome to Contacts"}
